ded009fd73794780fb9f024ad92ec0cd0b812dee
RajatThakur-27 <142075008+RajatThakur-27@users.noreply.github.com>
2024-09-27 17:47:53 +0530

    Update server.js

--- api/config/server.js
+++ api/config/server.js
@@ -1,18 +1,18 @@
-// module.exports = ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-//   app: {
-//     keys: env.array('APP_KEYS'),
-//   },
-//   webhooks: {
-//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-//   },
-// });
-
 module.exports = ({ env }) => ({
-  proxy: true,
-  url: env('HOST_URL'), // Sets the public URL of the application.
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', process.env.PORT),
   app: {
-    keys: env.array('APP_KEYS')
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
+
+// module.exports = ({ env }) => ({
+//   proxy: true,
+//   url: env('HOST_URL'), // Sets the public URL of the application.
+//   app: {
+//     keys: env.array('APP_KEYS')
+//   },
+// });
